9850c8506297f2e5666f15c41e1bce947ad92d08
Orbin-Acanto <oacanto@mmeink.com>
2025-10-15 18:06:10 -0400

    Update page.tsx

---
+++ src/app/services/rentals/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+import ServicesShowcase from '@/components/ServicesShowcase';
+import { eventCateringData } from '@/data';
+
+export default function EventRentalPage() {
+  return <ServicesShowcase {...eventCateringData} />;
+}
