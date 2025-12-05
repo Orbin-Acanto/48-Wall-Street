fc739ce75ea6f79502e2305f6258a9a17816d9dc
Orbin-Acanto <oacanto@mmeink.com>
2025-12-05 15:05:05 -0500

    Update index.tsx

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -3649,25 +3649,25 @@ export const CUSTOM_TABLE_COLORS = [
 ];
 
 export const tools: { id: Tool; label: string; icon: React.ReactElement }[] = [
-  {
-    id: 'select',
-    label: 'Select',
-    icon: (
-      <svg
-        className="h-5 w-5"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-        />
-      </svg>
-    ),
-  },
+  // {
+  //   id: 'select',
+  //   label: 'Select',
+  //   icon: (
+  //     <svg
+  //       className="h-5 w-5"
+  //       fill="none"
+  //       stroke="currentColor"
+  //       viewBox="0 0 24 24"
+  //     >
+  //       <path
+  //         strokeLinecap="round"
+  //         strokeLinejoin="round"
+  //         strokeWidth={2}
+  //         d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
+  //       />
+  //     </svg>
+  //   ),
+  // },
   // {
   //   id: 'wall',
   //   label: 'Wall',
